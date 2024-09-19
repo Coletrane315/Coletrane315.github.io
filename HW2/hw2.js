c2fdf1b79b1ed500d8f2c66a358dcbd61a1d374a
Coletrane315 <xcoletrane315x@gmail.com>
2024-09-19 02:25:49 -0400

    hw2 done

--- HW2/hw2.js
+++ HW2/hw2.js
@@ -2,16 +2,16 @@
 //Koch Mountains
 
 "use strict";
+var points = []; //global points array, could also just pass the values back up recursively
 var canvas = document.getElementById("gl-canvas");
 var gl = canvas.getContext('webgl2');
-var points = [vec2(-1, 0), vec2(1, 0)];
 var sliderElem = document.getElementById("slider");
 var sliderDisplayVal = document.getElementById("sliderLabelVal");
-var recursiveSteps = sliderElem.value;
+var recursiveSteps = sliderElem.value; //how many recursive steps to take, reads value from slider, defaults to 0
 
-init(points, gl.LINES);
+init();
 
-function init(pointsArr, mode){
+function init(){
     if (!gl) {alert("WebGL isn't available");}
     
     //Configure WebGL
@@ -21,43 +21,45 @@ function init(pointsArr, mode){
     //Load shaders and initialize attribute buffers
     let program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
+        
+    kochRecursive(vec2(-1, 0), vec2(1, 0), sliderElem.value); //starts recursive loop with initial endpoints
 
     //Load the data into the GPU
     let bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArr), gl.STATIC_DRAW);
-
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+        
     //Associate out shader variables with our data buffer
     let positionLoc = gl.getAttribLocation(program, "aPosition");
     gl.vertexAttribPointer(positionLoc , 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
-
-    render(mode);
+        
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.LINES, 0, points.length);
+    points = []; //clears points array
 };
 
-function render(renderMode){
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(renderMode, 0, points.length);
-}
-
+//called onChange to the slider, updates the display label, then renders again
 function refresh(){
     sliderDisplayVal.innerHTML = sliderElem.value;
-    kochRecursive(points[0], points[1], sliderElem.value);
+    init();
 }
 
 function kochRecursive(start, end, recSteps){
+    //If at the bottom of recursion, just draw horizontal line
     if(recSteps == 0){
-        let tmpPts = [vec2(start.x, start.y), vec2(start.x + (end.x - start.x) / 3, start.y), 
-            vec2(start.x + (end.x - start.x) / 2, start.y + (end.x - start.x) * Math.sqrt(3) / 3),
-            vec2(start.x + (end.x - start.x) * 2 / 3, start.y), vec2(end.x, end.y)];
-        init(tmpPts, gl.LINE_STRIP);
+        points.push(start);
+        points.push(end);
         return;
     }
 
-    kochRecursive(start, (start.x + end.x) / 3, recSteps - 1);
-    kochRecursive((start.x + end.x) * 2 / 3, end.x, recSteps - 1);
+    kochRecursive(start, vec2(start[0] + (end[0] - start[0]) / 3, start[1]), recSteps - 1); //Left-side recursion
 
+    points.push(vec2(start[0] + (end[0] - start[0]) / 3, start[1])); //endpoint of bottom left of triangle
+    points.push(vec2(start[0] + (end[0] - start[0]) / 2, start[1] + (end[0] - start[0]) * Math.sqrt(3) / 6)); //endpoint of top of triangle
+    //Since I am using the LINES primitive instead of LINE_STRIP, we need a second copy of this point
+    points.push(vec2(start[0] + (end[0] - start[0]) / 2, start[1] + (end[0] - start[0]) * Math.sqrt(3) / 6)); //endpoint of top of triangle
+    points.push(vec2(start[0] + (end[0] - start[0]) * 2 / 3, start[1])); //endpoint of bottom right of triangle
 
-
-
+    kochRecursive(vec2(start[0] + (end[0] - start[0]) * 2 / 3, start[1]), vec2(end[0], end[1]),recSteps - 1); //Right-side recursion
 }
